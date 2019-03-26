f4650e38d874e820cb421561362708c4f069de91
RevenantX <doomrevx@gmail.com>
2019-03-26 20:33:09 +0200

    fix collision

--- Assets/Code/Shared/Collisions.cs
+++ Assets/Code/Shared/Collisions.cs
@@ -8,9 +8,22 @@ namespace Code.Shared
             float cy = player.Position.y;
             float distX = x2-x1;
             float distY = y2-y1;
-            float dot = ( (cx-x1)*distX + (cy-y1)*distY ) / (distX*distX + distY*distY);
-            distX = x1 + dot * distX - cx;
-            distY = y1 + dot * distY - cy;
+            float lineLenSqr = distX * distX + distY * distY;
+            float dot = ( (cx-x1)*distX + (cy-y1)*distY ) / lineLenSqr;
+            float closestX = x1 + dot * distX;
+            float closestY = y1 + dot * distY;
+
+            float dcx1 = closestX - x1;
+            float dcy1 = closestY - y1;
+            float dcx2 = closestX - x2;
+            float dcy2 = closestY - y2;
+            float distToLineSqr1 = dcx1 * dcx1 + dcy1 * dcy1;
+            float distToLineSqr2 = dcx2 * dcx2 + dcy2 * dcy2;
+            if (distToLineSqr1 > lineLenSqr || distToLineSqr2 > lineLenSqr)
+                return false;
+            
+            distX = closestX - cx;
+            distY = closestY - cy;
             return distX*distX + distY*distY <= BasePlayer.Radius * BasePlayer.Radius;
         }
     }
